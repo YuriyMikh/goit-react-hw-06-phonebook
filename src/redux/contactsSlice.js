13b896d4c249950cc1e7d27e70a6e38731fc90e2
YuriyMikh <pamukkale1717@gmail.com>
2023-07-08 23:57:20 +0300

    upd

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,11 +1,13 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contactsInitialState = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
 
 const contactsSlice = createSlice({
   name: 'contacts', //имя слайса
@@ -16,7 +18,7 @@ const contactsSlice = createSlice({
     addContact: {
       //действия, которые бдут происходить в редюсере
       reducer(state, action) {
-        state.push(action.payload);
+        state.contacts.push(action.payload);
       },
       //будет подготавливать данные для записи
       prepare(name, number) {
@@ -31,7 +33,9 @@ const contactsSlice = createSlice({
     },
     //второй редюсер
     deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
     },
   },
 });
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,3 @@
-export const selectContacts = state => state.contacts; //получаем необходимую часть состояния
+export const selectContacts = state => state.contacts.contacts; //получаем необходимую часть состояния
 
 export const selectFilteredData = state => state.filter.value; //получаем необходимую часть состояния
