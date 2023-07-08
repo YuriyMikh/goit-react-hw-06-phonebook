665d030efb406b04e478e7adaf7f39abad5a002b
YuriyMikh <pamukkale1717@gmail.com>
2023-07-08 20:54:09 +0300

    upd

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
     filter: filterReducer,
   },
   //если проект задеплоен на продакшн - не показывай devTools.
   // devTools: process.env.NODE_ENV !== 'production',
 });
+
+
